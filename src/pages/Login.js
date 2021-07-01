49f8c99cf15489f2dcf0a10f127191887a3f99b3
Cameron Lucas <44956222+CamSkiTheDev@users.noreply.github.com>
2021-06-30 19:01:27 -0600

    adds form state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,19 @@
 import { Link } from 'react-router-dom'
 
+import { useState } from 'react'
+
 export default function Login() {
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	})
+
+	const handleChange = (e) =>
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}))
+
 	return (
 		<div className='bg-green-500 w-full min-h-screen flex items-center justify-center'>
 			<div className='p-4 bg-white rounded'>
@@ -9,14 +22,20 @@ export default function Login() {
 					<input
 						className='my-2 rounded w-full'
 						type='email'
+						name='email'
 						placeholder='Email Address'
 						required
+						value={formData.email}
+						onChange={handleChange}
 					/>
 					<input
 						className='my-2 rounded w-full'
 						type='password'
+						name='password'
 						placeholder='Password'
 						required
+						value={formData.password}
+						onChange={handleChange}
 					/>
 					<Link className='text-blue-600 underline'>
 						Don't have an account?
